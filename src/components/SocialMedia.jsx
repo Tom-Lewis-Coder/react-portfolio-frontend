e7eb14b6ea9036f3069b24116fe44bfd857d9659
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-11-30 16:35:42 +0000

    added CW link

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { SiCodewars } from 'react-icons/si'
 
 const SocialMedia = () => (
   <div className="app__social">
@@ -8,6 +9,9 @@ const SocialMedia = () => (
     </div>
     <div>
       <BsLinkedin onClick={() => window.open('https://www.linkedin.com/in/tomlewis139', '_blank')}/>
+    </div>
+    <div>
+      <SiCodewars onClick={() => window.open('https://www.codewars.com/users/Tom-Lewis-Coder', '_blank')}/>
     </div>
   </div>
 )

53d96c108b1498352c49fbaa25bd20cf2b461d33
Tom-Lewis-Coder <tomlewis139@gmail.com>
2023-01-25 16:22:28 +0000

    added background image to header and updated color scheme to match

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className='app__header-img'
       >
-        <img src={images.profile2} alt='profile__bg' />
+        <img src={images.profile2} className='hero__circle' alt='profile__bg' />
         <motion.img 
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: 'easeInOut' }}

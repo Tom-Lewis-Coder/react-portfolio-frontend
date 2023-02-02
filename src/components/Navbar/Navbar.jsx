8ec207ca96496020a95fe8b2ad2fbdf9e4bbe6c1
Tom Lewis <tomlewis139@gmail.com>
2023-02-02 10:15:44 +0000

    adjusted navbar toggle menu to fit/work on all devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,11 +25,11 @@ const Navbar = () => {
             </ul>
 
             <div className='app__navbar-menu'>
-                <HiMenuAlt4 onClick={() => setToggle(true)} />
+                <HiMenuAlt4 onClick={() => setToggle(!toggle)} />
 
                 {toggle && (
                     <motion.div
-                        whileInView={{ x: [300, 0] }}
+                        whileInView={{ x: [200, 0] }}
                         transition={{ duration: 0.85, ease: 'easeOut' }}
                     >
                         <HiX onClick={() => setToggle(false)} />
